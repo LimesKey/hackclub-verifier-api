baec11d4b199b8e960cf561fe26cf71a5b3a8f39
LimesKey <limeskeys@gmail.com>
2024-08-29 20:26:18 -0400

    eureka!!!

--- src/lib.rs
+++ src/lib.rs
@@ -12,26 +12,31 @@ use worker::*;
 #[event(fetch)]
 pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
     if req.method() == Method::Get {
-        let user = handle_oauth(req, env).await.unwrap();
-        let ysws_status = ysws_api(&user.user).await;
+        console_log!("GET request received");
+        let auth = handle_oauth(req, env).await;
+
+        console_log!("Getting user identity with: {}", &auth.authed_user.access_token);
+        let username = user_identity(&auth.authed_user.access_token).await;
+
+        console_log!("YSWS Status");
+        let ysws_status = ysws_api(&auth).await;
 
         let mut url = Url::parse("https://forms.hackclub.com/t/9yNy4WYtrZus").unwrap();
-        url.query_pairs_mut().append_pair("slack_id", &user.user.id);
+        url.query_pairs_mut().append_pair("slack_id", &auth.authed_user.id);
         url.query_pairs_mut()
             .append_pair("eligibility", &ysws_status.to_string());
         url.query_pairs_mut()
-            .append_pair("slack_user", &user.user.name);
-
-        Response::redirect(url)
+            .append_pair("slack_user", &username);
+        console_log!("Redirecting to {}", url);
+        Response::redirect_with_status(url, 302)
     } else {
         Response::error("Method Not Allowed", 405)
     }
 }
 
 // Handle the OAuth flow after the user has been redirected
-async fn handle_oauth(req: Request, env: Env) -> Result<OAuthResponse> {
-    // Parse the query parameters from the request
-    let url = req.url()?;
+async fn handle_oauth(req: Request, env: Env) -> OAuthResponse {
+    let url = req.url().unwrap();
     let params: QueryParams = match serde_qs::from_str(url.query().unwrap()) {
         Ok(params) => params,
         Err(_) => panic!("Error parsing query parameters"),
@@ -52,13 +57,12 @@ async fn handle_oauth(req: Request, env: Env) -> Result<OAuthResponse> {
     };
 
     console_log!("Client ID: {}", client_id);
-    console_log!("Redirect URI: {}", redirect_uri);
     console_log!("Code: {}", params.code);
 
     // Exchange authorization code for an access token
+    console_log!("Exchange code for token response");
     let access_token_response =
         exchange_code_for_token(&client_id, &client_secret, &params.code, &redirect_uri).await;
-
     return access_token_response;
 }
 
@@ -72,19 +76,14 @@ pub struct QueryParams {
 #[derive(Deserialize, Debug)]
 pub struct OAuthResponse {
     pub ok: bool,
-    pub user: User,
-    pub team: Team,
+    pub access_token: String,
+    pub authed_user: User,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct User {
-    pub name: String,
     pub id: String,
-}
-
-#[derive(Deserialize, Debug)]
-pub struct Team {
-    pub id: String,
+    pub access_token: String,
 }
 
 pub enum YSWSStatus {
@@ -105,17 +104,15 @@ impl fmt::Display for YSWSStatus {
     }
 }
 
-// Function to exchange authorization code for an access token
 pub async fn exchange_code_for_token(
     client_id: &str,
     client_secret: &str,
     code: &str,
     redirect_uri: &str,
-) -> Result<OAuthResponse> {
-    let client = Client::new();
+) -> OAuthResponse {
+    let client: Client = Client::new();
 
-    // Make the request to the Slack API to exchange the code for an access token
-    let request = client.post("https://slack.com/api/users.identity").form(&[
+    let request = client.post("https://slack.com/api/oauth.v2.access").form(&[
         ("client_id", client_id),
         ("client_secret", client_secret),
         ("code", code),
@@ -124,17 +121,24 @@ pub async fn exchange_code_for_token(
 
     let response = request.send().await.unwrap();
 
-    let oauth_response = response.json::<OAuthResponse>().await.unwrap();
+    // Clone the response bytes so you can log and parse the response
+    let response_bytes = response.bytes().await.unwrap();
 
-    Ok(oauth_response)
+    // Log the response as text
+    console_log!("Response: {:?}", String::from_utf8_lossy(&response_bytes));
+
+    // Parse the response as JSON
+    let oauth_response: OAuthResponse = serde_json::from_slice(&response_bytes).unwrap();
+    oauth_response
 }
 
-async fn ysws_api(user_id: &User) -> YSWSStatus {
+
+async fn ysws_api(user: &OAuthResponse) -> YSWSStatus {
     let client = Client::new();
     let url = "https://verify.hackclub.dev/api/status";
 
     let json_body: Value = serde_json::json!({
-        "slack_id": user_id.id,
+        "slack_id": user.authed_user.id,
     });
 
     // Create headers
@@ -157,4 +161,30 @@ async fn ysws_api(user_id: &User) -> YSWSStatus {
         "Insufficient" => YSWSStatus::Insufficient,
         _ => YSWSStatus::Ineligible,
     }
+}
+
+async fn user_identity(access_token: &String) -> String {
+    let client = Client::new();
+    let url = "https://slack.com/api/openid.connect.userInfo";
+    console_log!("Getting user info with access token: {}", access_token);
+
+    let response = client.get(url).bearer_auth(access_token).send().await.unwrap();
+
+    let response_bytes = response.bytes().await.unwrap();
+
+    // Log the response as text
+    console_log!("Response: {:?}", String::from_utf8_lossy(&response_bytes));
+
+    // Parse the response as JSON
+    let user_info: UserInfo = serde_json::from_slice(&response_bytes).unwrap();
+
+    user_info.name
+}
+
+
+#[derive(Deserialize, Debug)]
+struct UserInfo {
+    ok: bool,
+    name: String,
+    email: String,
 }
