3321e9898e4894f876ffebd5bac2f00e065b1fb1
LimesKey <limeskeys@gmail.com>
2024-09-02 21:33:59 -0400

    add hashing validation

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,12 @@ use reqwest::{
 use serde::Deserialize;
 use serde_json::Value;
 use serde_qs;
-use std::fmt;
+use std::{fmt, hash};
 use totp_rs::{Algorithm, Secret, TOTP};
 use wasm_timer::UNIX_EPOCH;
+use serde_json::json;
+use std::hash::{DefaultHasher, Hash, Hasher};
 use worker::*;
-
-mod utils;
 
 cfg_if! {
     if #[cfg(feature = "console_log")] {
@@ -29,10 +29,16 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
     init_log();
 
+
+    let client_secret = match env.var("SLACK_CLIENT_SECRET") {
+        Ok(var) => var.to_string(),
+        Err(_) => panic!("Client secret not set"),
+    };
+
     if req.method() == Method::Get {
         console_log!("GET request received");
 
-        let auth = handle_oauth(req, env).await;
+        let auth = handle_oauth(req.url().unwrap(), &env).await;
         console_log!("Fetched Bot and User Token");
 
         console_log!(
@@ -51,38 +57,49 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
         let slack_username = username.clone();
         let ysws_status = ysws_status.to_string().clone();
 
-        let secret = slack_id + &slack_username + &ysws_status;
+        let secret = slack_id + &slack_username + &ysws_status + &client_secret.to_string();
 
-        let totp = TOTP::new(
-            Algorithm::SHA256,
-            6,
-            1,
-            300,
-            Secret::Raw(secret.as_bytes().to_vec())
-                .to_bytes()
-                .expect("Failed to convert secret to bytes"),
-            Some("hackclub-ysws-verifier".to_string()),
-            slack_username.clone(),
-        )
-        .unwrap();
+        // let totp = TOTP::new(
+        //     Algorithm::SHA256,
+        //     6,
+        //     1,
+        //     300,
+        //     Secret::Raw(secret.as_bytes().to_vec())
+        //         .to_bytes()
+        //         .expect("Failed to convert secret to bytes"),
+        //     Some("hackclub-ysws-verifier".to_string()),
+        //     slack_username.clone(),
+        // )
+        // .unwrap();
 
-        let token = totp.generate(
-            wasm_timer::SystemTime::now()
-                .duration_since(UNIX_EPOCH)
-                .unwrap()
-                .as_secs(),
-        );
+        // let token = totp.generate(
+        //     wasm_timer::SystemTime::now()
+        //         .duration_since(UNIX_EPOCH)
+        //         .unwrap()
+        //         .as_secs(),
+        // );
+        let mut hasher: DefaultHasher = DefaultHasher::new();
+        secret.hash(&mut hasher);
+        let hashed_secret = hasher.finish().to_string();
 
         // Redirecting to the form with the slack_id and eligibility status
 
         let mut url = Url::parse("https://forms.hackclub.com/t/9yNy4WYtrZus").unwrap(); // fillout form URL
-        url.query_pairs_mut().append_pair("secret", &token);
+        url.query_pairs_mut().append_pair("secret", &hashed_secret.to_string());
         url.query_pairs_mut()
             .append_pair("slack_id", &auth.authed_user.id);
         url.query_pairs_mut()
             .append_pair("eligibility", &ysws_status.to_string());
         url.query_pairs_mut().append_pair("slack_user", &username);
         console_log!("Redirecting to {}", url);
+
+        let records = get_records(&env).await;
+        console_log!("Records fetched");
+        match records.is_empty() {
+            true => console_log!("No records to verify"),
+            false => verify_otp(records, env).await,
+        }
+
         Response::redirect_with_status(url, 302)
     } else {
         Response::error("Method Not Allowed", 405)
@@ -90,8 +107,7 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
 }
 
 // Handle the OAuth flow to get the access token/ slack id
-async fn handle_oauth(req: Request, env: Env) -> OAuthResponse {
-    let url = req.url().unwrap();
+async fn handle_oauth(url: Url, env: &Env) -> OAuthResponse {
     let params: QueryParams = match serde_qs::from_str(url.query().unwrap()) {
         Ok(params) => params,
         Err(_) => panic!("Error parsing query parameters"),
@@ -248,30 +264,128 @@ struct UserInfo {
     email: String,
 }
 
-// fn verify_airtable() {
+async fn get_records(env: &Env) -> Vec<Record> {
+    let client = Client::new();
+    let url = "http://hackclub-ysws-api.jasperworkers.workers.dev/submissions";
 
-// }
+    let jasper_api = match env.var("JASPER_API") {
+        Ok(var) => var.to_string(),
+        Err(_) => panic!("Jasper API not set"),
+    };
 
-// async fn get_airtable_records() -> Vec<Record<OnBoardRecord>> {
-//     let airtable = Airtable::new_from_env();
-//     // Get the current records from a table.
-//     let mut records: Vec<Record<OnBoardRecord>> = airtable
-//         .list_records(
-//             "Submissions",
-//             "Pending",
-//             vec!["OTP"],
-//         )
-//         .await
-//         .unwrap();
+    let response = client
+        .get(url)
+        .bearer_auth(jasper_api)
+        .send()
+        .await
+        .unwrap();
 
-//     // Iterate over the records.
-//     for (i, record) in records.clone().iter().enumerate() {
-//         println!("{} - {:?}", i, record);
-//     }
-//     return records;
-// }
+    let response_values: Vec<Value> = response.json::<Vec<Value>>().await.unwrap();
+    let mut records: Vec<Record> = Vec::new();
+        
+    for value in response_values {
+        match serde_json::from_value::<Record>(value.clone()) {
+            Ok(record) => {
+                records.push(record);
+            }
+            Err(e) => {
+                console_log!("Skipping invalid record: {:?}", value);
+                console_log!("Error: {:?}", e);
+            }
+        }
+    }
+    records
+}
 
-// #[derive(Debug, Clone, Serialize, Deserialize)]
-// struct OnBoardRecord {
-//     OTP: String,
-// }
+async fn verify_otp(records: Vec<Record>, env: Env) {
+    console_log!("Looking into {} records", records.len());
+
+    for record in records {
+        let otp_secret = record.fields.otp;
+        let eligibility = record.fields.eligibility;
+        let slack_id = record.fields.slack_id;
+        let slack_username = record.fields.slack_username;
+
+        let client_secret = match env.var("SLACK_CLIENT_SECRET") {
+            Ok(var) => var.to_string(),
+            Err(_) => panic!("Client secret not set"),
+        };
+
+        let jasper_api = match env.var("JASPER_API") {
+            Ok(var) => var.to_string(),
+            Err(_) => panic!("Jasper API not set"),
+        };
+
+        let secret = slack_id + &slack_username + &eligibility + &client_secret.to_string();
+
+        let mut hasher = DefaultHasher::new();
+        secret.hash(&mut hasher);
+        let hashed_secret = hasher.finish().to_string();
+
+        let client = Client::new();
+        let url: Url = Url::parse("http://hackclub-ysws-api.jasperworkers.workers.dev/update").unwrap();
+        let bearer_token = jasper_api; // Replace with your actual Bearer token
+
+        if hashed_secret == otp_secret {
+            
+            let json_body = json!({
+                "recordId": record.id,
+                "authenticated": "true",
+            });
+        
+            let response = client
+                .post(url)
+                .bearer_auth(bearer_token)
+                .json(&json_body)
+                .send()
+                .await.unwrap();
+        
+            if response.status().is_success() {
+                console_log!("Request successful");
+            } else {
+                console_log!("Request failed with status: {}", response.status());
+            }
+        } else {
+            console_warn!("OTP mismatch");
+
+            let json_body = json!({
+                "recordId": record.id,
+                "authenticated": "false",
+            });
+
+            let response = client
+                .post(url)
+                .bearer_auth(bearer_token)
+                .json(&json_body)
+                .send()
+                .await.unwrap();
+
+            if response.status().is_success() {
+                console_log!("Request successful");
+            } else {
+                console_log!("Request failed with status: {}", response.status());
+            }
+        }
+    }
+}
+
+
+#[derive(Deserialize, Debug)]
+struct Record {
+    id: String,
+    #[serde(rename = "createdTime")]
+    created_time: String,
+    fields: Fields,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+struct Fields {
+    eligibility: String,
+    #[serde(rename = "OTP")]
+    otp: String,
+    #[serde(rename = "Slack ID")]
+    slack_id: String,
+    #[serde(rename = "SlackUsername")]
+    slack_username: String,
+}
