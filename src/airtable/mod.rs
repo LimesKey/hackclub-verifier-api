96a0d0d87598f81e81e69b8983a620b502d0f902
LimesKey <limeskeys@gmail.com>
2024-11-17 19:43:22 -0500

    small change
    Signed-off-by: LimesKey <limeskeys@gmail.com>

--- src/airtable/mod.rs
+++ src/airtable/mod.rs
@@ -6,4 +6,5 @@ pub mod verify;
 pub use fetch::fetch_submissions;
 pub use types::{Fields, Record};
 pub use update::update_submission;
-pub use verify::*;
+pub use verify::{process_verify_records_request, verify_all_records, initiate_record_verification};
+
