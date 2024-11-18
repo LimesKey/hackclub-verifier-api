20d44fb439c57cdf9ea6af6c8cb1f159a675bbde
LimesKey <limeskeys@gmail.com>
2024-11-17 19:47:34 -0500

    test2

--- src/airtable/mod.rs
+++ src/airtable/mod.rs
@@ -6,4 +6,6 @@ pub mod verify;
 pub use fetch::fetch_submissions;
 pub use types::{Fields, Record};
 pub use update::update_submission;
-pub use verify::*;
+pub use verify::{
+    initiate_record_verification, process_verify_records_request, verify_all_records,
+};
